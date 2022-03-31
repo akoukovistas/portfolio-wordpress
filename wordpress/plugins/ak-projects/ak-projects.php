0ec7410dedde80c18b50c5bca67a7eadcaab2e8f
Alexandros <alexandros@koukovistas.com>
2022-04-01 00:57:15 +0200

    Add basic plugin and CPT

---
+++ wordpress/plugins/ak-projects/ak-projects.php
@@ -0,0 +1,27 @@
+<?php
+defined( 'ABSPATH' ) or die();
+
+/**
+ * Plugin Name:     Ak Projects
+ * Plugin URI:      https://alexandros.work
+ * Description:     Custom Projects Showcase
+ * Author:          Alexandros Koukovistas
+ * Author URI:      https://alexandros.work
+ * Text Domain:     ak-projects
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Ak_Projects
+ */
+
+
+define( 'AKP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'AKP_PLUGIN_NAME', trim( dirname( SIN_PLUGIN_BASENAME ), '/' ) );
+define( 'AKP_PLUGIN_DIR', untrailingslashit( dirname( __FILE__ ) ) );
+define( 'AKP_PLUGIN_URL', untrailingslashit( plugins_url( '', __FILE__ ) ) );
+
+include AKP_PLUGIN_DIR . '/inc/project-posttype.php';
+
+add_action('init', function(){
+	new ProjectsPostType();
+},0);
